6bb0c183549739eaf9cbc03eb1693c5b9e323786
Andrew Krug <andrewkr@osfAashland.org>
2015-04-22 10:53:32 -0700

    moified start script for fedora4 and solr latest

--- bin/start-docker-production.sh
+++ bin/start-docker-production.sh
@@ -3,8 +3,11 @@
 #Spin up the maria database
 docker run -d --name hydraproductiondb andrewkrug/mariadb
 
-#Spin up jetty
-docker run -d --name hydraproductionjetty andrewkrug/hydrajetty
+#Spin up fedora4
+docker run -d --name hydraproductionfedora andrewkrug/fedora4
+
+#Spin up solr-latest
+docker run -d --name hydraproductionsolr andrewkrug/solr
 
 #Spin up osfsufia
-docker run -d -p 80:80 -P --name osfsufiaproduction --link hydraproductiondb:hydraproductiondb --link hydraproductionjetty:hydraproductionjetty  andrewkrug/osfdigitalarchive
+docker run -d -p 80:80 -P --name osfsufiaproduction --link hydraproductiondb:hydraproductiondb --link hydraproductionfedora:hydraproductionfedora --link hydraproductionsolr:hydraproductionsolr andrewkrug/osfdigitalarchive
