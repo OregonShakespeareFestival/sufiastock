1aeb4c113cde72847652165b70ed463950b48948
Joel Ferrier <jferrier45@gmail.com>
2015-04-23 15:03:37 -0700

    change startup script order

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /rails
 rake db:seed
+redis-server &
+nginx -c /rails/config/container/nginx.conf -g "pid /rails/tmp/pids/nginx.pid;" &
 bundle exec unicorn -c /rails/config/container/unicorn.rb -E production
-nginx -c /rails/config/container/nginx.conf -g "pid /rails/tmp/pids/nginx.pid;"
-redis-server
