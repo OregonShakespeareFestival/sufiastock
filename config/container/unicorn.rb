bfecc4d3695861d791a6b4a9482dc92daa2a0342
Joel Ferrier <jferrier45@gmail.com>
2015-04-23 14:17:28 -0700

    remove old comments

--- config/container/unicorn.rb
+++ config/container/unicorn.rb
@@ -1,27 +1,22 @@
-#Unicorn Config Modifed by Krug
-#from digital ocean
+#Unicorn Config Modifed by Ferrier
 #https://www.digitalocean.com/community/tutorials/how-to-deploy-rails-apps-using-unicorn-and-nginx-on-centos-6-5
 
 # Set the working application directory
-# working_directory "/path/to/your/app"
-working_directory "/rails"
-#
+working_directory "/rails/app"
+
 # Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/rails/unicorn.pid"
-#
+pid "/rails/tmp/pids/unicorn_master.pid"
+
 # Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
 stderr_path "/rails/log/unicorn.err.log"
 stdout_path "/rails/log/unicorn.log"
-#
+
 # Unicorn socket
 listen "/rails/unicorn.sufia.sock"
-#
+
 # Number of processes
 worker_processes 4
-#
+
 # Time-out
 timeout 600
 
